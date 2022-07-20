f2d44c9eefe5a577230323893f3d8a0d3927ff0e
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-20 20:03:46 +0200

    Update add-2-digits-of-a-2-digit-number.py

--- add-2-digits-of-a-2-digit-number.py
+++ add-2-digits-of-a-2-digit-number.py
@@ -1,5 +1,7 @@
 two_digit_number = input("Type a two digit number: ")
+print(type(two_digit_number))
 
+#subscripting - pulling chars from str
 a=two_digit_number[0]
 b=two_digit_number[1]
 print(int(a) + int(b))
